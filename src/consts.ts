5978ecb84a2cfa2e1c1fff38670c4925995f4466
shuent <shunshun.43@gmail.com>
2025-07-02 02:31:13 +0900

    udpate

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Shuent の dev Blog';
-export const SITE_DESCRIPTION = 'Welcome to my blog';
+export const SITE_TITLE = "shuent Blog"
+export const SITE_DESCRIPTION = "shuent Blog"
